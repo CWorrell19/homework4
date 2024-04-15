8d67847173eed7cb84b0fe18a4dba8e3c269a201
CWorrell19 <164057541+CWorrell19@users.noreply.github.com>
2024-04-14 21:43:18 -0700

    Create Personnel.java

---
+++ Personnel.java
@@ -0,0 +1,10 @@
+// Conner Worrell
+
+// Firm class to demonstrate polymorphism via inheritance
+public class Firm {
+    // Main method to create a staff of employees for a firm and pay them
+    public static void main(String[] args) {
+        Staff personnel = new Staff();
+        personnel.payday();
+    }
+}
